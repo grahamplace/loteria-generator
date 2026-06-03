d756d1cc2d1443cdbe0065517fe4473039714bbf
Graham Place <grahamgplace@gmail.com>
2026-06-03 12:59:58 -0700

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,5 +2,5 @@ export const FREE_CARD_LIMIT = 4;
 export const TOTAL_CARD_COUNT = 54;
 
 // Single source of truth for board unlock pricing. Update both values together.
-export const BOARD_UNLOCK_PRICE_CENTS = 2000;
-export const BOARD_UNLOCK_PRICE_DISPLAY = '$20';
+export const BOARD_UNLOCK_PRICE_CENTS = 3500;
+export const BOARD_UNLOCK_PRICE_DISPLAY = '$35';
